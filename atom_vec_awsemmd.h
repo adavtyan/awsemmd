d9413595b7b1ff229441b7aecc7d7352c022ed7e
gpapoian <gpapoian@gmail.com>
2016-05-04 18:27:42 -0400

    adding virtual keywords to atom_vec_awsemmd.h

--- atom_vec_awsemmd.h
+++ atom_vec_awsemmd.h
@@ -28,34 +28,34 @@ class AtomVecAWSEM : public AtomVec {
  public:
   AtomVecAWSEM(class LAMMPS *);
   virtual ~AtomVecAWSEM() {}
-  void grow(int);
-  void grow_reset();
-  void copy(int, int, int);
+  virtual void grow(int);
+  virtual void grow_reset();
+  virtual void copy(int, int, int);
   virtual int pack_comm(int, int *, double *, int, int *);
   virtual int pack_comm_vel(int, int *, double *, int, int *);
   virtual void unpack_comm(int, int, double *);
   virtual void unpack_comm_vel(int, int, double *);
-  int pack_reverse(int, int, double *);
-  void unpack_reverse(int, int *, double *);
+  virtual int pack_reverse(int, int, double *);
+  virtual void unpack_reverse(int, int *, double *);
   virtual int pack_border(int, int *, double *, int, int *);
   virtual int pack_border_vel(int, int *, double *, int, int *);
-  int pack_border_hybrid(int, int *, double *);
+  virtual int pack_border_hybrid(int, int *, double *);
   virtual void unpack_border(int, int, double *);
   virtual void unpack_border_vel(int, int, double *);
-  int unpack_border_hybrid(int, int, double *);
+  virtual int unpack_border_hybrid(int, int, double *);
   virtual int pack_exchange(int, double *);
   virtual int unpack_exchange(double *);
-  int size_restart();
-  int pack_restart(int, double *);
-  int unpack_restart(double *);
-  void create_atom(int, double *);
-  void data_atom(double *, imageint, char **);
-  int data_atom_hybrid(int, char **);
-  void pack_data(double **);
-  int pack_data_hybrid(int, double *);
-  void write_data(FILE *, int, double **);
-  int write_data_hybrid(FILE *, double *);
-  bigint memory_usage();
+  virtual int size_restart();
+  virtual int pack_restart(int, double *);
+  virtual int unpack_restart(double *);
+  virtual void create_atom(int, double *);
+  virtual void data_atom(double *, imageint, char **);
+  virtual int data_atom_hybrid(int, char **);
+  virtual void pack_data(double **);
+  virtual int pack_data_hybrid(int, double *);
+  virtual void write_data(FILE *, int, double **);
+  virtual int write_data_hybrid(FILE *, double *);
+  virtual bigint memory_usage();
 
   tagint *residue; 
 
